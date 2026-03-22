7314581ddd41c963533c303aca44799ded1cd3d3
yuji0276 <yujikami0110@gmail.com>
2026-03-22 20:37:10 +0900

    chore:site_descriptionの変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = 'kmy blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'プログラミングや技術について発信するブログ';
 export const SITE_AUTHOR = 'kmy';
 export const GOOGLE_SITE_VERIFICATION = 'Tsi36eMGcvG9prREdzIzBNkYY5vmwXv9JX7Ju9YWhuA';
 
